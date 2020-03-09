36378e69622b89f641277662a2c4e7145da877b5
zzy <zhuoya.zhao@hand-china.com>
2020-03-09 12:29:05 +0800

    update 配置antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Button } from "antd";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
